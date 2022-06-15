c557952a6ef671a0bebcad42b682787a3defc4cf
TegHocine <arcou98@hotmail.com>
2022-06-15 22:03:18 +0100

    header comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from './components/header/Header'
+import Game from './components/game/Game'
+import Rules from './components/rules/Rules'
 import './App.scss'
 
 function App() {
-  return <div className='App'>hello</div>
+  return (
+    <div className='App'>
+      <Header />
+      <Game />
+      <Rules />
+    </div>
+  )
 }
 
 export default App
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import Logo from '../../assets/logo.svg'
+
 import './header.scss'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className='header'>
+      <div className='header__title'>
+        <img src={Logo} alt='rock paper logo' />
+      </div>
+      <div className='header__score'>
+        <div className='title'>Score</div>
+        <div className='number'>12</div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
