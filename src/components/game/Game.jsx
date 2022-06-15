f1512c5a2e97ae701a7a928a0ed635937004e197
TegHocine <arcou98@hotmail.com>
2022-06-15 20:49:37 +0100

    ui skeleton

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './game.scss'
+
+const Game = () => {
+  return <div>Game</div>
+}
+
+export default Game
---
+++ src/components/rules/Rules.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './rules.scss'
+
+const Rules = () => {
+  return <div>Rules</div>
+}
+
+export default Rules
